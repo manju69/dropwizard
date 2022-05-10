11a5df84e6cde9b800a57e9715fd24201e3b153a
manju69 <87670779+manju69@users.noreply.github.com>
2022-05-09 23:41:50 -0400

    resource modified

--- src/main/java/repository/GeolocationDAO.java
+++ src/main/java/repository/GeolocationDAO.java
@@ -4,6 +4,8 @@ import domain.Geolocation;
 import io.dropwizard.hibernate.AbstractDAO;
 import org.hibernate.SessionFactory;
 
+import javax.persistence.Query;
+
 public class GeolocationDAO extends AbstractDAO<Geolocation> {
     public GeolocationDAO(SessionFactory sessionFactory){
         super(sessionFactory);
@@ -15,5 +17,11 @@ public class GeolocationDAO extends AbstractDAO<Geolocation> {
 
     public void save(Geolocation geolocation) {
         persist(geolocation);
+    }
+
+    public Geolocation getFromQuery(String ipAddress){
+        String geoquery = "select * from geolocation where query ="+ipAddress;
+        Query query = currentSession().createQuery(geoquery);
+        return (Geolocation) query.getSingleResult();
     }
 }
